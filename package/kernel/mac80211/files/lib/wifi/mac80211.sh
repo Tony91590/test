1875b6d0da7c6da7f70b58a510793395f3122a3a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2022-01-21 17:38:28 +0100

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -168,10 +168,8 @@ detect_mac80211() {
 			set wireless.radio${devidx}.band=${mode_band}
 			set wireless.radio${devidx}.htmode=$htmode
 			set wireless.radio1.htmode=HE160
-			set wireless.radio1.he_bss_color=8
-			set wireless.radio2.he_bss_color=8
-			set wireless.radio1.he_su_beamformee=1
-			set wireless.radio2.he_su_beamformee=1
+			set wireless.radio1.mu_beamformer=1
+			set wireless.radio2.mu_beamformer=1
 			set wireless.radio${devidx}.disabled=0
 			set wireless.radio0.disabled=1
 			set wireless.radio${devidx}.country=US
